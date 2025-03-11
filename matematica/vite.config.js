7d17e7beb08a668d35cdb4bc85170b7ab55e988a
Ticoliro08 <ggsantana18@gmail.com>
2025-03-10 23:36:40 -0300

    att4

--- matematica/vite.config.js
+++ matematica/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // Alterando o diretório de saída para 'build'
+    outDir: 'dist' // Mantenha o padrão
   }
 })
